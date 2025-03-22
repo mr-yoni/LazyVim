a64d74e81a90a5e55aaa42776c4fa40bc047e97a
Yoni Madar <mr.yoni.madar@gmail.com>
2025-03-22 22:48:01 +0200

    update my LazyVim config

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- vim.cmd('let $PATH = "/Users/yonimadar/.nvm/versions/node/v20.15.0/bin:" . $PATH')
+
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- local g = vim.g
+-- g.python3_host_prog = "~/.pyenv/versions/3.11.3/envs/neovim3/bin/python3"
+-- g.loaded_python3_provider = 1
+--
+-- g.node_host_prog = "/Users/yonimadar/.nvm/versions/node/v20.15.0/bin/neovim-node-host"
