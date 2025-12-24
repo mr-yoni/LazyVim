6cf9438f859aa7ae682f80d17e681eb2ad29990c
Yoni Madar <mr.yoni.madar@gmail.com>
2025-12-24 11:30:25 +0200

    try adding telescope keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,28 @@ map("n", "<Up>", "<Nop>")
 map("n", "<Down>", "<Nop>")
 map("n", "<Right>", "<Nop>")
 map("n", "<Left>", "<Nop>")
+
+require("telescope").setup({
+  -- .. other settings
+  extensions = {
+    file_browser = {
+      -- hijack_netrw = true,
+      theme = "ivy",
+      mappings = {
+        i = {
+          ["<C-y>"] = function()
+            local entry = require("telescope.actions.state").get_selected_entry()
+            local cb_opts = vim.opt.clipboard:get()
+            if vim.tbl_contains(cb_opts, "unnamed") then
+              vim.fn.setreg("*", entry.path)
+            end
+            if vim.tbl_contains(cb_opts, "unnamedplus") then
+              vim.fn.setreg("+", entry.path)
+            end
+            vim.fn.setreg("", entry.path)
+          end,
+        },
+      },
+    },
+  },
+})
