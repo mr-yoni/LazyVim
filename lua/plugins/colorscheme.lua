5218af6bd83460832b83e85a75edff47491b5e9e
Yoni Madar <yonim@alice.io>
2026-04-16 13:06:55 +0300

    add colorscheme auto dark mode

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- Auto theme detection
+  {
+    "f-person/auto-dark-mode.nvim",
+    priority = 1000,
+    config = function()
+      local auto_dark_mode = require("auto-dark-mode")
+
+      auto_dark_mode.setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.api.nvim_set_option_value("background", "dark", {})
+          vim.cmd("colorscheme tokyonight")
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option_value("background", "light", {})
+          vim.cmd("colorscheme habamax")
+        end,
+      })
+    end,
+  },
+}
