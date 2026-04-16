d41f5c74742cdfd46777c92ff0698ce493c95393
Yoni Madar <yonim@alice.io>
2026-04-16 13:10:30 +0300

    use habamax

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,7 +10,7 @@ return {
         update_interval = 1000,
         set_dark_mode = function()
           vim.api.nvim_set_option_value("background", "dark", {})
-          vim.cmd("colorscheme tokyonight")
+          vim.cmd("colorscheme habamax")
         end,
         set_light_mode = function()
           vim.api.nvim_set_option_value("background", "light", {})
