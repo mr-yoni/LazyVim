e7184bc7a9ecbeb5d836b061d8a68cecd6b4ba2d
Yoni Madar <yonim@alice.io>
2026-04-16 13:30:15 +0300

    use default colorscheme

--- init.lua
+++ init.lua
@@ -3,12 +3,3 @@
 -- vim.cmd('let $PATH = "/Users/yonimadar/.nvm/versions/node/v20.15.0/bin:" . $PATH')
 
 require("config.lazy")
-
-vim.schedule(function()
-  -- This forces Neovim to respect the background after the UI has settled
-  if vim.o.background == "dark" then
-    vim.cmd.colorscheme("habamax")
-  else
-    vim.cmd.colorscheme("habamax")
-  end
-end)
